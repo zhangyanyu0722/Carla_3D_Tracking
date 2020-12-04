90cda59b7ba6d403fb65018fd7d0e94bbc551bfb
YANYU ZHANG <zhangya@bu.edu>
2020-12-04 00:18:47 -0500

    Add files via upload

---
+++ faster-rcnn.pytorch/init.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+set -ex
+
+_PWD=$(pwd)
+_DATA_GTA=${_PWD}'/../3d-tracking/data/gta5_tracking/'
+_DATA_KITTI_T=${_PWD}'/../3d-tracking/data/kitti_tracking/'
+_DATA_KITTI_D=${_PWD}'/../3d-tracking/data/kitti_object/'
+_DATA_CARLA=${_PWD}'/../3d-tracking/data/carla/'
+
+
+# make .so files
+cd lib/
+python setup.py build develop
+cd ${_PWD}
+
+# download pretrain model
+mkdir -p data/pretrained_model
+cd data/pretrained_model
+wget https://s3.amazonaws.com/pytorch/models/resnet101-5d3b4d8f.pth
+mv resnet101-5d3b4d8f.pth resnet101_caffe.pth
+cd ${_PWD}
+
+# Link GTA, KITTI and CARLA dataset to data
+# get gta data
+cd data/
+ln -s ${_DATA_GTA} gta5_tracking
+echo "mv GTA dataset (train, val, test) to data/gta5_tracking"
+
+# get kitti data
+cd ${_PWD}/data/
+ln -s ${_DATA_KITTI_T} kitti_tracking
+echo "mv KITTI tracking dataset (train, test) to data/kitti_tracking"
+
+# get carla data
+cd ${_PWD}/data/
+ln -s ${_DATA_CARLA} carla
+echo "link CARLA dataset (train, val, test) to data/kitti_tracking"
+
+# If you downloaded detection dataset, uncomment the following two lines
+#ln -s ${_DATA_KITTI_D} kitti_object 
+#echo "mv KITTI detection dataset (train, test) to data/kitti_object"
+
+# create folder vis/ (detection output) and models (model weights)
+cd ${_PWD}
+mkdir vis/
+mkdir -p models/res101/gta_det/
+mkdir models/res101/kitti/
+
+# (optional) Copy a detection pretrained model
+echo "Please move faster_rcnn_200_14_18895.pth to models/res101/gta_det/"
+echo "Please move faster_rcnn_300_100_175.pth to models/res101/kitti/"
